f871c2fbf624a20e20bfe0083b3c7fa50005fed9
Rahul <rahul.khakse@gmail.com>
2020-08-28 22:27:25 +0530

    created blank screen files
    added ui images
    added icon package

---
+++ aliexpress/src/constants/themes.js
@@ -0,0 +1,16 @@
+const colors={
+    
+    white:'#f7f7f7',
+    red:'#df2e05',
+    grey:'#d6d6d6',
+    green:'#5abe42',
+    orange:'#ff6600',
+    blue:'#157dfa',
+    
+    background_app:'#eeeeee',
+    inactive_tab:'#444444',
+    active_tab:'#e52e04',
+    navigation_bar:'#fafafa',
+}
+
+export default colors;
